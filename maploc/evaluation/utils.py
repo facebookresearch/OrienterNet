e1263e0bbdd0592759b9a7849866d6291e5ab4bd
Alan Savio Paul <alpaul@student.ethz.ch>
2024-03-19 18:57:30 +0100

    Refactor refactor_pred

--- maploc/evaluation/utils.py
+++ maploc/evaluation/utils.py
@@ -1,7 +1,5 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
-from copy import deepcopy
-
 import numpy as np
 import torch
 from omegaconf import OmegaConf
@@ -44,14 +42,14 @@ def write_dump(output_dir, experiment, cfg, results, metrics):
     write_json(output_dir / "log.json", dump)
 
 
-def swap_uv_ij(uv, im_height=256):
+def swap_uv_ij(uv, im_height=255):
     if uv is not None:
         ij = uv.clone()
         ij[..., 1] = im_height - uv[..., 1]
         return ij
 
 
-def refactor_model_output(batch_, pred_):
+def refactor_pred(pred):
     """
     Changes yaw convention
     Changes uv indices (origin top left) to ij indices (origin bottom left)
@@ -59,39 +57,25 @@ def refactor_model_output(batch_, pred_):
              uv_expectation, uvr_expectation, yaw_expectation -> Transform2D
     Changes map memory layout to allow indexing like map[i, j]
     """
-    batch = deepcopy(batch_)
-    pred = deepcopy(pred_)
 
     # changing yaw angle from north-clockwise to east-counterclockwise
-    def adjust_yaw(angle):
-        return 90 - angle
+    yaw_max = 90 - pred["yaw_max"][..., -1:][None]
+    yaw_expectation = 90 - pred["yaw_expectation"][None]
+    if "yaw_fused" in pred:
+        pred["yaw_fused"] = 90 - pred["yaw_fused"][None]
 
-    # change ij_gt + yaw to Transform2D object map_T_query
-    yaw_gt = adjust_yaw(batch["roll_pitch_yaw"][..., -1])
-    ij_gt = swap_uv_ij(batch["uv"])
-    batch["map_T_query_gt"] = Transform2D.from_degrees(yaw_gt[None], ij_gt)
-    batch.update(
-        {
-            "ij_gps": swap_uv_ij(batch.get("uv_gps")),
-            "ij_init": swap_uv_ij(batch["uv_init"]),
-        }
-    )
-    del batch["uv"], batch["uv_gps"], batch["uv_init"], batch["roll_pitch_yaw"]
-
-    yaw_max = adjust_yaw(pred["yaw_max"][..., -1:])[None]
     ij_max = swap_uv_ij(pred["uv_max"])
-    yaw_expectation = adjust_yaw(pred["yaw_expectation"])[None]
     ij_expectation = swap_uv_ij(pred["uv_expectation"])
-    pred["map_T_query_max"] = Transform2D.from_degrees(yaw_max, ij_max)
-    pred["map_T_query_expectation"] = Transform2D.from_degrees(
+    if "uv_fused" in pred:
+        pred["ij_fused"] = swap_uv_ij(pred["uv_fused"])
+        del pred["uv_fused"]
+
+    pred["map_T_cam_max"] = Transform2D.from_degrees(yaw_max, ij_max)
+    pred["map_T_cam_expectation"] = Transform2D.from_degrees(
         yaw_expectation, ij_expectation
     )
     del pred["yaw_max"], pred["uv_max"], pred["yaw_expectation"], pred["uv_expectation"]
     del pred["uvr_max"], pred["uvr_expectation"]
-
-    batch.update(
-        {k: torch.rot90(batch[k], -1, dims=(-2, -1)) for k in ["map", "map_mask"]}
-    )
 
     pred["map"].update(
         {
@@ -115,12 +99,6 @@ def refactor_model_output(batch_, pred_):
         {k: torch.rot90(pred[k], -1, dims=(-3, -2)) for k in ["scores", "log_probs"]}
     )
 
-    if "uv_fused" in pred:
-        pred["ij_fused"] = swap_uv_ij(pred["uv_fused"])
-        del pred["uv_fused"]
-    if "yaw_fused" in pred:
-        pred["yaw_fused"] = adjust_yaw(pred["yaw_fused"])[None]
-
     if "log_probs_fused" in pred:
         pred["log_probs_fused"] = torch.rot90(
             pred["log_probs_fused"], -1, dims=(-3, -2)
@@ -130,4 +108,14 @@ def refactor_model_output(batch_, pred_):
             pred["scores_unmasked"], -1, dims=(-3, -2)
         )
 
-    return batch, pred
+    return pred
+
+
+def refactor_batch(batch):
+
+    # Ideally, this happens in the dataloader.
+    batch.update(
+        {k: torch.rot90(batch[k], -1, dims=(-2, -1)) for k in ["map", "map_mask"]}
+    )
+
+    return batch
